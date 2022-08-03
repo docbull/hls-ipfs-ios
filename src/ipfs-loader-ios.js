f84947455f55720978543323746fa8ccb85d4fb7
docbull <fudghks21@gmail.com>
2022-08-03 23:36:43 +0900

    streaming logs are deleted

--- src/ipfs-loader-ios.js
+++ src/ipfs-loader-ios.js
@@ -1,12 +1,5 @@
 import { HlsIpfsLoader } from './hls-ipfs-loader.js';
 
-let frozen = 0;
-let cnt = 0;
-
-setInterval(() => {
-  cnt++;
-}, 1000);
-
 // initIPFS gets video chunks from IPFS using it's own CID. 
 async function initIPFS(CID) {
   Hls.DefaultConfig.loader = HlsIpfsLoader;
@@ -19,15 +12,10 @@ async function initIPFS(CID) {
     hls.loadSource('master.m3u8');
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED, () => {
-      console.log(hls.stats);
       video.play();
     });
-    video.addEventListener('waiting', (event) => {
-      frozen++;
-      console.log(`Frozen event is triggered at ${cnt}; ${frozen}`);
-    });
+
   } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    // let videoSource = await loadChunk(`${CID}/master.m3u8`);
     let videoSource = '';
     // var ms = new MediaSource();
     // video.src = window.URL.createObjectURL(ms);
